d5ad10983235e12dcf5633b34245858fcccc3283
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 01:05:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "Пупкин";
+			string lastName = "Вася";
+			Console.WriteLine($"первоначальное значение name - {name}; первоначальное значение lastName - {lastName}");
+			string exchange = lastName;
+			lastName = name;
+			name = exchange;
+			Console.WriteLine($"теперь значение name - {name}; теперь значение lastName - {lastName}");
+		}
+	}
+}
